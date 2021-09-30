99b27e7e7cd196b2f1781e770af0cfc33849e4cb
HarrisonIT <hsbc.itpartner@gmail.com>
2021-09-30 02:02:41 -0500

    Eliminacion de node y reinstalacion - Se agrega el tema re react router con los links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import 'styles/App.css'
 import Index from 'pages';
 import Siberiano from "pages/Siberiano"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
@@ -9,14 +9,14 @@ function App() {
       <Router>
         <Switch>
 
-          <Route path="/index">
-            <Index />
-          </Route>
-          
           <Route path="/Siberiano">
             <Siberiano />
           </Route>
 
+          <Route path="/">
+            <Index />
+          </Route>
+          
         </Switch>
       </Router>
     </div>
--- src/components/Cardraza.jsx
+++ src/components/Cardraza.jsx
@@ -1,9 +1,13 @@
+import {Link} from "react-router-dom"
+
 function Cardraza ({raza , imagen}) {
     return (
       <li className="bread-card">
+        <Link to = "/Siberiano">
         <div className="contenedor-imagen">
           <img src={imagen} alt= {raza} />
         </div>
+          </Link>
         <span className="bread-title"> {raza}
         </span>
       </li>
